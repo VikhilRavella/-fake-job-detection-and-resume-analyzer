756ee306448cf4654692bbd50de58452afa2b6b3
RAVELLA VIKHIL <164651332+VikhilRavella@users.noreply.github.com>
2026-02-09 00:57:10 +0530

    Update settings.py

--- job_verifier_project/job_verifier_project/settings.py
+++ job_verifier_project/job_verifier_project/settings.py
@@ -134,9 +134,14 @@ USE_TZ = True
 # =========================
 # STATIC FILES
 # =========================
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
