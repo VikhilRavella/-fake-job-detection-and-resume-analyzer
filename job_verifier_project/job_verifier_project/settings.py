9541e14993c296f83232bd51920423ecbc8802ae
RAVELLA VIKHIL <164651332+VikhilRavella@users.noreply.github.com>
2026-02-09 01:24:35 +0530

    Remove static file settings from settings.py
    Removed STATIC_ROOT and STATICFILES_STORAGE settings.

--- job_verifier_project/job_verifier_project/settings.py
+++ job_verifier_project/job_verifier_project/settings.py
@@ -143,10 +143,8 @@ STATICFILES_DIRS = [
 ]
 
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+S
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
