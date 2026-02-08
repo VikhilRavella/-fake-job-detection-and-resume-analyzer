70dd8c672cf38e28a774c3e905310df316379e26
RAVELLA VIKHIL <164651332+VikhilRavella@users.noreply.github.com>
2026-02-09 00:38:24 +0530

    Update urls.py

--- job_verifier_project/main_app/urls.py
+++ job_verifier_project/main_app/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     # Homepage
-    path('', views.home_view, name='home'),
+    
 
     # ❌ ML / Heavy routes DISABLED for Render free tier
     # path('predict/', views.manual_predict_view, name='predict'),
     # path('agent/', views.agent_view, name='agent'),
 
-    # Static HTML pages (SAFE)
-    path('about/', views.about_view, name='about'),
-    path('contact/', views.contact_view, name='contact'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
